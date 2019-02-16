a9d02eb1f525e1c66af089674b0827399252a35c
student <devinm@csedge.org>
2019-02-16 15:47:17 -0800

    Initial commit

---
+++ app/src/main/java/com/teamedge/android/chicagobullshalloffame/MainActivity.java
@@ -0,0 +1,23 @@
+package com.teamedge.android.chicagobullshalloffame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void openTopScoringPlayers(View view) {
+        Intent i = new Intent(this, TopScoringPlayerActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/teamedge/android/chicagobullshalloffame/TopScoringPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.teamedge.android.chicagobullshalloffame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopScoringPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_scoring_players);
+    }
+}
